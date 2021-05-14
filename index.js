3a586cc69074c84f35d01e86800b21217c5ed664
Mohammed Mahmood <mohammed.hamidf99@gmail.com>
2021-05-14 13:34:14 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Add your code here
+
+const submitData = (name, email) => {
+    return fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify({
+      name: name,
+      email: email,
+    })
+  }).then((response)=>{
+    return response.json()
+  }).then((json)=>{
+    console.log(json);
+    const body = document.getElementsByTagName('body')[0];
+    const p = document.createElement('p')
+    p.innerHTML = `${json.id}`
+    body.append(p);
+  }).catch((message)=>{
+    const body = document.getElementsByTagName('body')[0];
+    const p = document.createElement('p')
+    p.innerHTML = message
+    body.append(p);
+  });
+}
+submitData("mohammed", "hamid");
