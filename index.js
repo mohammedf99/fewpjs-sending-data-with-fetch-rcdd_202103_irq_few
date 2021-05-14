c85cf35cdf2a73ac292596cec2693961616e6c73
Mohammed Mahmood <mohammed.hamidf99@gmail.com>
2021-05-14 13:33:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,24 +1,29 @@
 // Add your code here
 
-const submitData = (userName, userEmail) => {
-  const URL = "http://localhost:3000/dogs";
-
-  return fetch(URL, {
+const submitData = (name, email) => {
+    return fetch("http://localhost:3000/users", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
       "Accept": "application/json"
     },
     body: JSON.stringify({
-      name: userName,
-      email: userEmail
-    }).then((response) => response.json()).then((json) => {
-        const body = document.getElementsByTagName('body')[0];
-        const p = document.createElement('p')
-        p.innerHTML = `${json.id}`
-        body.append(p);
-    }).catch((error) => console.log(error.message))
+      name: name,
+      email: email,
+    })
+  }).then((response)=>{
+    return response.json()
+  }).then((json)=>{
+    console.log(json);
+    const body = document.getElementsByTagName('body')[0];
+    const p = document.createElement('p')
+    p.innerHTML = `${json.id}`
+    body.append(p);
+  }).catch((message)=>{
+    const body = document.getElementsByTagName('body')[0];
+    const p = document.createElement('p')
+    p.innerHTML = message
+    body.append(p);
   });
 }
-
 submitData("mohammed", "hamid");
