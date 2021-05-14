898ce092f1f18fcaf2d3c97b3a14cf09ecf8a557
Mohammed Mahmood <mohammed.hamidf99@gmail.com>
2021-05-14 13:28:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,14 +3,14 @@
 const submitData = () => {
   const URL = "http://localhost:3000/dogs";
 
-fetch(URL, {
-  method: "POST",
-  headers: {
-    "Content-Type": "application/json",
-    "Accept": "application/json"
-  },
-  body: JSON.stringify({
-    
-  }).then((response) => response.json()).catch((error) => console.log(error.message))
-});
+  fetch(URL, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify({
+      
+    }).then((response) => response.json()).catch((error) => console.log(error.message))
+  });
 }
