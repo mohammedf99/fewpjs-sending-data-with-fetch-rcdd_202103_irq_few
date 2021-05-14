b9a22f6cd346e91cbea37f5581cb2aa1c0a525d7
Mohammed Mahmood <mohammed.hamidf99@gmail.com>
2021-05-14 13:13:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Add your code here
+
+const URL = "http://localhost:3000/dogs";
+
+fetch(URL, {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+    "Accept": "application/json"
+  }
+  body: JSON.stringify({
+    name: "Steve",
+    email: "steve@steve.com"
+  });
+})
